0df1075140efb4d6bfc92559b4a3a3f5a05a63ce
Abhinav2k <abhinavkalathilbiju82@gmail.com>
2026-09-20 21:15:23 +0530

    Widget: white themed Skip button and black themed Cancel button (v1.4.8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vibeschedule.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.4.7"
+        versionCode = 12
+        versionName = "1.4.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
