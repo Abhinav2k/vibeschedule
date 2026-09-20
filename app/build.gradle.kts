e55389ac5b0d400aeb9aa653645d152790126c78
Abhinav2k <abhinavkalathilbiju82@gmail.com>
2026-09-20 19:39:20 +0530

    v1.4.3 - Refine widget to signature iPhone liquid glass with lighter translucent frosted glass and specular sheen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vibeschedule.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.4.2"
+        versionCode = 7
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
