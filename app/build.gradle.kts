d40b28d54d9ecdfcd0c4b27bc07b5d4c2ea539ef
Abhinav2k <abhinavkalathilbiju82@gmail.com>
2026-09-20 20:41:19 +0530

    Adjust widget padding: push big buttons to outer edges (v1.4.6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vibeschedule.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.4.5"
+        versionCode = 10
+        versionName = "1.4.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
