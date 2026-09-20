a7346c7401d25533d35c226de0000814fcfc61e8
Abhinav2k <abhinavkalathilbiju82@gmail.com>
2026-09-20 19:49:06 +0530

    v1.4.4 - Perfectly round circular widget buttons + prismatic diffraction & optical blur glass background

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vibeschedule.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.4.3"
+        versionCode = 8
+        versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
